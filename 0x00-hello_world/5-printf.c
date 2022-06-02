52f1d7d9cd2274619f69c24bc6948aedb9fe3ace
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-02 05:00:22 -0700

    Fixed error in the code for Hello, printf task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,14 @@
 /*
 *
 *main - Entry point
+*
 *print string using printf
+*
 *Return: Always 0 (Success)
 */
 int main(void)
 {
+
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
