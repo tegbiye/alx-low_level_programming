500d9cf5c3424de50b75d311824f4e80652989ee
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-03 07:53:13 -0700

    Added task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
