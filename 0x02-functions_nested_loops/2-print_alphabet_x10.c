9f5f0c6a06eca76244768efcf08164315e0de960
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-07 09:37:01 -0700

    Added task 2 10X alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+char max;
+for (max = 0; max <= 9; max++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
