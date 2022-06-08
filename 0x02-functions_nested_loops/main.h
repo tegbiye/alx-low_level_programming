bfe087700e032c3524d03b9efcf4a676a0f92625
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-08 05:08:29 -0700

    Added task 11 Battery street

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _SCHOOL_H_
-#define _SCHOOL_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,6 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 
-#endif /* _SCHOOL_H_ */
+#endif /* _MAIN_H_ */
 
