60ae05b531fd685fa19821a179da8bc90a519cb1
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-08 05:14:33 -0700

    Fixed the error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,24 +12,17 @@ void print_to_98(int n)
 int i;
 if (n < 98)
 {
-for (i = 0; i <= 98; i++)
+for (i = 0; i <= 97; i++)
 {
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
-}
+printf("%d, ", i);
 }
 }
 else
 {
-for (i = n; i >= 98; i--)
+for (i = n; i > 98; i--)
 {
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
+printf("%d, ", i);
 }
 }
-}
+printf("98\n");
 }
