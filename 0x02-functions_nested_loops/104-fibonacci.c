37ad02bbf1473b4232c348e7ed33cb227b6c3462
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-08 08:43:10 -0700

    Fixed error task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,24 +6,46 @@
   */
 int main(void)
 {
-unsigned long i, a0, a1, sum;
+int i;
+unsigned long a0, a1, sum, m, n, p, q;
 a0 = 0;
 a1 = 1;
+i = 0;
 
-for (i = 0; i < 98; i++)
+for (i = 1; i <= 91; i++)
 {
 sum = a0 + a1;
 a0 = a1;
 a1 = sum;
-printf("%lu", sum);
-if (i == 97)
+printf("%lu, ", sum);
+}
+m = a0 % 1000;
+a0 = a0 / 1000;
+n = a1 % 1000;
+a1 = a1 / 1000;
+while (i <= 98)
 {
-printf("\n");
+q = (m + n) / 1000;
+p = (m + n) - q * 1000;
+sum = (a0 + a1) + q;
+m = n;
+n = p;
+a0 = a1;
+a1 = sum;
+if (p >= 100)
+{
+printf("%lu%lu", sum, p);
 }
 else
 {
+printf("%lu0%lu", sum, p);
+}
+if (i != 98)
+{
 printf(", ");
 }
+i++;
 }
+printf("\n");
 return (0);
 }
