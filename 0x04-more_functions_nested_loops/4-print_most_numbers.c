9c983d0a14285f150afda818c5db90cb5cf918f0
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-09 09:40:35 -0700

    Fixed error in task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,15 @@
 void print_most_numbers(void)
 {
 int i;
-for (i = 48; i <= 57; i++)
+for (i = 0; i <= 9; i++)
 {
-if (i != 50 && i != 53)
+if (i == 2 || i == 4)
 {
-putchar(i);
+continue
 }
 else
 {
-continue;
+putchar(i + '0');
 }
 }
 putchar('\n');
