437274c4be2a452302e44b357e48bf0f57a72d03
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-13 08:48:01 -0700

    Fixed error in task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,16 @@
  * Return: print n array of integers
  */
 
+
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n - 1; i++)
+
+for (i = 0; i < (n - 1); i++)
 {
 printf("%d, ", a[i]);
-if ( i == (n - 1))
-{
+}
 printf("%d", a[n-1]);
-}
-}
 printf("\n");
 }
+
