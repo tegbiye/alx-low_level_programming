852ef7e407410cb59c227b219698b134b2b40f61
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-13 05:35:31 -0700

    Fixed error in styling

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+
 /**
- * reset_to_98: takes a pointer to an int as parameter
- *@*n: takes as a parameter
- *
- *Return: void
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: n
  */
+
+
 void reset_to_98(int *n)
 {
 
