d1a301a6b1530b88aa2a2f7591a7d9d76a843273
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-16 09:32:04 -0700

    Added task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * rot13 - a function that encodes a string using rot13e
+  * @s: The char array argument
+  *
+  * Return: char var
+  */
+
+
+char *rot13(char *s)
+{
+int a = 0;
+
+while (s[a])
+{
+while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+{
+if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+{
+s[a] -= 13;
+break;
+}
+s[a] += 13;
+break;
+}
+
+a++;
+}
+
+return (s);
+}
