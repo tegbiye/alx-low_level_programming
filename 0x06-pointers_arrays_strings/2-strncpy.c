1f5837b72ea4cb752f0258ceff3a3b00897b30ee
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-16 03:32:14 -0700

    Fixed error in task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,27 +5,29 @@
  * char *_strncpy - a function that copies a string
  * @dest: dest char argument
  * @src:  src argument
- * @n: n argument
- * Return: string
+ * @n: n argument limit
+ * Return: char value
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int len = 0,i = 0;
-while (src[i])
+int i = 0, j = 0;
+while (src[j])
 {
-i++;
-}
-while (src[len] && len < n)
-{
-dest[len] = src[len];
-len++;
-}
-while (len < n)
-{
-dest[len] = '\0';
-len++;
+j++;
 }
 
+while (i < n && src[i])
+{
+dest[i] = src[i];
+i++;
+}
+
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
 return (dest);
+
 }
