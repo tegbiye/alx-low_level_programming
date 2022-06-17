c154dffe0469936355fa60a66dd955d18aa2090a
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-17 05:56:02 -0700

    Added task 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+int j, k;
+j = 0;
+while (j <= 9)
+{
+if (j <= s)
+	printf("%02x", c[l * 10 + j]);
+else
+	printf("  ");
+if (j % 2)
+	putchar(' ');
+j++;
+}
+k = 0;
+while (k <= s)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+	putchar(c[l * 10 + k]);
+else
+	putchar('.');
+k++;
+}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+int i;
+i = 0;
+while (i <= (size - 1) / 10 && size)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+i++;
+}
+if (size == 0)
+	putchar('\n');
+}
