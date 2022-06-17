bf29b17228975224508eaece9b5b02660f7395c2
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-17 01:10:25 -0700

    Fixed error in task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,63 +2,59 @@
 #include <stdlib.h>
 
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
-  *
-  * Return: Nothing
-  */
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l, m, n;
-while (n1[i] && n2[i])
-{
-;
-i++;
-j++;
-}
-if (i > size_r || j > size_r)
-{
-return (0);
-}
-m = 0;
-k = 0;
-while (k < size_r - 1)
-{
-i -= 1;
-j -= 1;
-n = m;
-if (i >= 0)
-{
-n += n1[i] - '0';
-}
-if (j >= 0)
-{
-n += n2[j] - '0';
-}
-if (i < 0 && j < 0 && n == 0)
-{
-break;
-}
-m = n / 10;
-r[k] = n % 10 + '0';
-i--;
-j--;
-k++;
-}
-r[k] = '\0';
-if (i >= 0 || j >= 0 || m)
-{
-return (0);
-}
-l = 0;
-while (l < k)
-{
-k -= 1;
-m = r[k];
-r[k] = r[l];
-r[l] = m;
-k--;
-l++;
-}
-return (r);
+	int i, j, k, l, m, n;
+	i = 0;
+	j = 0;
+	while (n1[i])
+	{
+	;
+	i++;
+	}
+	while (n2[j])
+	{
+	;
+	j++;
+	}
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	k = 0;
+	for (i -= 1, j -= 1; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+	{
+		return (0);
+	}
+	l = 0;
+	for (k -= 1; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
