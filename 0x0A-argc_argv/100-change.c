808ce1295f06a49067f75a46713bfac50d9e51fa
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-24 06:43:56 -0700

    Fixed error by making other function converter

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 
 /**
+ * converter - function that is used in the main
+ *@i: int
+ *
+ *Return: the number of coins
+ */
+
+int converter(int i)
+{
+int count = 0;
+
+while (i != 0)
+{
+if (i % 10 == 9 || i % 10 == 7)
+	i -= 2;
+else if (i % 25 == 0)
+	i -= 25;
+else if (i % 10 == 0)
+	i -= 10;
+else if (i % 5 == 0)
+	i -= 5;
+else if (i % 2 == 0)
+{
+if (i % 10 == 6)
+	i -= 1;
+else
+	i -= 2;
+}
+else
+	i -= 1;
+count++;
+}
+
+return (count);
+}
+
+
+/**
  * main - prints the minimum number of coins
  *@argc: int
  *@argv: char
@@ -12,7 +49,7 @@
 int main(int argc, char *argv[])
 {
 int i;
-int count = 0;
+int coin = 0;
 if (argc != 2)
 {
 printf("Error\n");
@@ -23,28 +60,9 @@ if (i < 0)
 	printf("0\n");
 else
 {
-while (i != 0)
-{
-if (i % 10 == 9 || i % 10 == 7)
-	i = i - 2;
-else if (i % 25 == 0)
-	i = i - 25;
-else if (i % 10 == 0)
-	i = i - 10;
-else if (i % 5 == 0)
-	i = i - 5;
-else if (i % 2 == 0)
-{
-if (i % 10 == 6)
-	i = i - 1;
-else
-	i = i - 2;
-}
-else
-	i = i - 1;
-++count;
-}
-printf("%d\n", count);
+coin = converter(i);
+
+printf("%d\n", coin);
 }
 return (0);
 
