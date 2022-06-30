5d066f9cbebfd3b88db646ed12d90674a63ceb0d
tegbiye <se.tegbabu.solomon@gmail.com>
2022-06-30 07:13:55 -0700

    Fixed the styling

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings using at most an inputted number of bytes.
+ * string_nconcat - Concatenates two strings
+ *using at most an inputted number of bytes.
  * @s1: The first string.
  * @s2: The second string.
  * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * Return: If the function fails - NULL Otherwise - a pointer to concatenated space in memory.
+ * Return: If the function fails - NULL
+ * Otherwise - a pointer to concatenated space in memory.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
