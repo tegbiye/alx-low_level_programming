360a75a4b7bda8e759bd4aaeca2276baf20e4610
tegbiye <se.tegbabu.solomon@gmail.com>
2022-07-04 06:54:04 -0700

    Fixed error in task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,15 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-printf("Name: %s\nAge: %6f\nOwner: %s\n", d->name, d->age, d->owner);
+if (!(d->name))
+	printf("Name: (nil)\n");
+else
+	printf("Name: %s\n", d->name);
 
+printf("Age: %f\n", d->age);
+if (!(d->owner))
+	printf("Owner: (nil)\n");
+else
+	printf("Owner: %s\n", d->owner);
 }
 }
