ee6940f52737279161829eb0561c4c6506dbf675
tegbiye <se.tegbabu.solomon@gmail.com>
2022-07-04 04:25:01 -0700

    Fixed error in styling

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
 printf("%s\n", __FILE__);
-return(0);
+return (0);
 }
