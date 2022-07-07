b47a5e541ade76cb7328cfad680ab06bf7a92f9f
tegbiye <se.tegbabu.solomon@gmail.com>
2022-07-07 00:15:48 -0700

    Fixed error in task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,21 +13,30 @@
 
 int main(int argc, char *argv[])
 {
-ud_t obj;
-int i = 0, j = 0;
+ud_t ud_obj;
+int val = 0, i = 0;
+
 if (argc == 2)
 {
-i = atoi(argv[1]);
-if (i < 0)
-	printf("Error\n");
-	exit(2);
-ud_unit(&obj);
-ud_set_input_buffer(&obj, argv[1], i);
-ud_set_mode(&obj, 64);
-ud_set_syntax(&obj, UD_SYN_INTEL);
+val = atoi(argv[1]);
 
-while (ud_disassemble(&obj))
-	printf("\t%s\n", ud_insn_hex(&obj));
+if (val < 0)
+{
+printf("Error\n");
+exit(2);
 }
+
+ud_unit(&ud_obj);
+ud_set_input_buffer(&ud_obj, argv[1], val);
+ud_set_mode(&ud_obj, 64);
+ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+while (ud_disassemble(&ud_obj))
+{
+printf("\t%s\n", ud_insn_hex(&ud_obj));
+}
+}
+
 return (0);
 }
+
