5320d92074157742c498d1a4245f560d9394d0d1
tegbiye <se.tegbabu.solomon@gmail.com>
2022-07-07 00:07:04 -0700

    Added task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <udis86.h>
+
+
+/**
+ * main - prints the opcodes of its own function
+ * @argc: int
+ * @argv: char
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+ud_t obj;
+int i = 0, j = 0;
+if (argc == 2)
+{
+i = atoi(argv[1]);
+if (i < 0)
+	printf("Error\n");
+	exit(2);
+ud_unit(&obj);
+ud_set_input_buffer(&obj, argv[1], i);
+ud_set_mode(&obj, 64);
+ud_set_syntax(&obj, UD_SYN_INTEL);
+
+while (ud_disassemble(&obj))
+	printf("\t%s\n",ud_insn_hex(&obj));
+}
+return (0);
+}
