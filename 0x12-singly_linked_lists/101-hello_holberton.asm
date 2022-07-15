643e5f785f5fe37a935d10b786d5c5850d12582f
tegbiye <se.tegbabu.solomon@gmail.com>
2022-07-14 22:56:37 -0700

    Added task 6 assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+; File: 101-hello_holberton.asm
+; Auth: Tegbabu S. Nuramo
+; Description:  64-bit program in assembly that
+;        prints Hello, Holberton, followed by a new line.
+
+extern printf
+
+section .txt
+	global main
+main:
+	push rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+pop rbp
+mov rax,0
+ret
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
