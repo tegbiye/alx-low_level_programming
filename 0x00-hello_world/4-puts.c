e9d29c146087291d8411198b4a980605fdb7a502
tegbiye <se.tegbabu.solomon@gmail.com>
2023-10-27 15:05:56 +0300

    Added task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - Entru point
+* Return: ALways 0 (Success)
+* Prints a string
+*/
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
