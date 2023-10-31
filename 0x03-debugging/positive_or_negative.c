58d7e668348e09d1ee239fa05d3cb817ee755628
tegbiye <se.tegbabu.solomon@gmail.com>
2023-10-31 12:50:28 +0300

    Added the task 3

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*positive_or_negative - Function takes arguement n
+*
+*Return: nothing just print
+*/
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
+
