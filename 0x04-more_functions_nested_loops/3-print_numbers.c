370bee602e2b996b9d1fb729d49ac3e808f01f53
tegbiye <se.tegbabu.solomon@gmail.com>
2023-11-02 12:33:47 +0300

    prints numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ *Return: none
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
