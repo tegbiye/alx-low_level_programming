378208d8f94b92253fc349afcb1de5431dce1c5e
tegbiye <se.tegbabu.solomon@gmail.com>
2023-11-15 09:16:38 +0300

    Added task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1 and on error -1
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
